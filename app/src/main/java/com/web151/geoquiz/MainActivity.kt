3c38ee707fead580d0fae821d8bec11c8a90dee7
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2025-01-13 15:54:27 -0500

    Initial commit. App is running with the list of quesitons and buttons working. Buttons have the old code commented out still.

---
+++ app/src/main/java/com/web151/geoquiz/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.web151.geoquiz
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
+import com.web151.geoquiz.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_asia, true),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_australia, true)
+    )
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+
+        binding.trueButton.setOnClickListener { view: View ->
+             checkAnswer(true)
+//            Snackbar.make(view, "That is correct!", Snackbar.LENGTH_SHORT)
+//                .setBackgroundTint(Color.rgb(0, 153, 0))
+//                .setDuration(1500)
+//                .show()
+        }
+
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
+//            Snackbar.make(view, "Sorry, that is incorrect.", Snackbar.LENGTH_SHORT)
+//                .setBackgroundTint(Color.rgb(204, 0, 0))
+//                .setDuration(1500)
+//                .show()
+        }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer:Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResId = if(userAnswer == correctAnswer){
+            Snackbar.make(binding.root, "That is correct!", Snackbar.LENGTH_SHORT)
+                .setBackgroundTint(Color.rgb(0, 153, 0))
+                .setDuration(1500)
+                .show()
+        } else {
+            Snackbar.make(binding.root, "Sorry, that is incorrect.", Snackbar.LENGTH_SHORT)
+                .setBackgroundTint(Color.rgb(204, 0, 0))
+                .setDuration(1500)
+                .show()
+        }
+    }
+}
---
+++ app/src/main/java/com/web151/geoquiz/Question.kt
@@ -0,0 +1,5 @@
+package com.web151.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question(@StringRes val textResId: Int, val answer: Boolean)
