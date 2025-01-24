add1cb5086b2f60c7a4dd5d59e29f731186f69c7
Cyber-Kaeh <153845457+Cyber-Kaeh@users.noreply.github.com>
2025-01-23 19:02:26 -0500

    App works if questions are from start to finish with no wrap

--- app/src/main/java/com/web151/geoquiz/MainActivity.kt
+++ app/src/main/java/com/web151/geoquiz/MainActivity.kt
@@ -2,30 +2,36 @@ package com.web151.geoquiz
 
 import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isInvisible
 import com.google.android.material.snackbar.Snackbar
 import com.web151.geoquiz.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     private val questionBank = listOf(
-        Question(R.string.question_asia, true),
-        Question(R.string.question_americas, true),
-        Question(R.string.question_africa, false),
-        Question(R.string.question_oceans, true),
-        Question(R.string.question_mideast, false),
-        Question(R.string.question_australia, true)
+        Question(R.string.question_asia, true, false, false),
+        Question(R.string.question_americas, true, false, false),
+        Question(R.string.question_africa, false, false, false),
+        Question(R.string.question_oceans, true, false, false),
+        Question(R.string.question_mideast, false, false, false),
+        Question(R.string.question_australia, true, false, false)
     )
     private var currentIndex = 0
+    private var correctAnswers = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate(Bundle?) called")    // Added log msg
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -34,11 +40,13 @@ class MainActivity : AppCompatActivity() {
         binding.questionTextView.setText(questionTextResId)
 
         binding.trueButton.setOnClickListener { view: View ->
-             checkAnswer(true)
+            checkAnswer(true)
+            checkQuestions()
         }
 
         binding.falseButton.setOnClickListener { view: View ->
             checkAnswer(false)
+            checkQuestions()
         }
 
         binding.questionTextView.setOnClickListener {
@@ -54,9 +62,9 @@ class MainActivity : AppCompatActivity() {
         binding.previousButton.setOnClickListener {
             currentIndex = if (currentIndex - 1 < 0) {
                 // wrap to last question
-                questionBank.size - 1
+//                questionBank.size - 1
                 // stop at first
-//                return@setOnClickListener
+                return@setOnClickListener
             } else {
                 (currentIndex - 1) % questionBank.size
             }
@@ -65,15 +73,53 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
+    }
+
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
         binding.questionTextView.setText(questionTextResId)
+        if (questionBank[currentIndex].isAnswered) {
+            binding.trueButton.isEnabled = false
+            binding.falseButton.isEnabled = false
+        } else {
+            binding.trueButton.isEnabled = true
+            binding.falseButton.isEnabled = true
+        }
+        checkQuestions()
     }
 
     private fun checkAnswer(userAnswer:Boolean){
         val correctAnswer = questionBank[currentIndex].answer
+        Log.d(TAG, "Current question index: $currentIndex")
+        questionBank[currentIndex].isAnswered = true
+        binding.trueButton.isEnabled = false
+        binding.falseButton.isEnabled = false
 
         val messageResId = if(userAnswer == correctAnswer){
+            correctAnswers++
             Snackbar.make(binding.root, "That is correct!", Snackbar.LENGTH_SHORT)
                 .setBackgroundTint(Color.rgb(0, 153, 0))
                 .setDuration(1500)
@@ -88,5 +134,24 @@ class MainActivity : AppCompatActivity() {
 
     private fun nextQuestion() {
         currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+    private fun checkQuestions() {
+        var allAnswered = false
+        for (i in questionBank){
+            if (i.isAnswered) {
+                allAnswered = true
+            } else {
+                allAnswered = false
+            }
+        }
+        if (allAnswered) {
+            Snackbar.make(binding.root,
+                "All questions answered! You got $correctAnswers right!",
+                Snackbar.ANIMATION_MODE_SLIDE)
+                .setBackgroundTint(Color.rgb(64, 64, 64))
+                .setDuration(5000)
+                .show()
+        }
     }
 }
--- app/src/main/java/com/web151/geoquiz/Question.kt
+++ app/src/main/java/com/web151/geoquiz/Question.kt
@@ -2,4 +2,5 @@ package com.web151.geoquiz
 
 import androidx.annotation.StringRes
 
-data class Question(@StringRes val textResId: Int, val answer: Boolean)
+data class Question(@StringRes val textResId: Int, val answer: Boolean,
+                    var isAnswered: Boolean, var isCorrect: Boolean)
